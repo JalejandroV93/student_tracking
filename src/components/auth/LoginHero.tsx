82a780caa2b6612f140a39560484953377a42763
JAVM93 <jhonvasquez93@utp.edu.co>
2025-12-03 13:06:50 -0500

    feat(LoginHero): añadir efecto de zoom a la imagen de fondo y actualizar el texto del encabezado

--- src/components/auth/LoginHero.tsx
+++ src/components/auth/LoginHero.tsx
@@ -35,8 +35,7 @@ export function LoginHero() {
           src="/bg-tracking2.webp"
           alt="Background"
           fill
-          objectFit="cover"
-          className="object-contain"
+          className="object-cover animate-zoomEffect"
           priority
         />
       </div>
@@ -55,7 +54,7 @@ export function LoginHero() {
           <Logo className="w-16 h-16 lg:w-20 lg:h-20 hidden" />
           <h2 className=" text-white leading-tight">
             <p className="text-2xl lg:text-3xl font-bold">SGC</p>
-            <p>Sistema Integral de Gestión Académica y Convivencial</p>
+            <p>Sistema de Gestión Convivencial</p>
           </h2>
         </motion.div>
       </div>
